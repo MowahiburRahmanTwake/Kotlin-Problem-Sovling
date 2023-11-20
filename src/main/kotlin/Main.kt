860f05c2c04ad91f84201c8cee793cc7f16ad27b
twake1234 <twake.edu@gmail.com>
2023-11-20 11:39:35 +0600

    add some code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
 fun main(args: Array<String>) {
-    print("Name: Md. Mowahibur Rahman Twake\nEmail: twake.edu@gmail.com")
+    print("Name: Md. Mowahibur Rahman Twake\nEmail: twake.edu@gmail.com\n")
+    print("Thank you!\n")
 }
