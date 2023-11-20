b39d27d535c2359f8f70e01d0d5bfca856ed1336
twake1234 <twake.edu@gmail.com>
2023-11-20 13:06:34 +0600

    add data type

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,46 @@
+//fun main() {
+//    var num:Byte = 127
+//    var num1:Byte = -128
+//
+//    var a = Byte.MAX_VALUE
+//    var a1 = Byte.MIN_VALUE
+//    println(a)
+//    println(a1 )
+//
+//}
+
+//fun main() {
+//    val template: Short = -11245
+//    println("$template")
+//
+//    var a = Short.MAX_VALUE
+//    var a1 = Short.MIN_VALUE
+//    println(a)
+//    print("$a1")
+//}
+
+
+fun main() {
+    var a:Int = Int.MAX_VALUE
+    var b:Int = Int.MIN_VALUE
+//    var a = Int.MAX_VALUE
+//    var b = Int.MIN_VALUE
+
+    print("The max value ${a} and the minimum value ${b}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
-fun main(args: Array<String>) {
-    print("Name: Md. Mowahibur Rahman Twake\nEmail: twake.edu@gmail.com\n")
-    print("Thank you!\n")
-}
+//fun main(args: Array<String>) {
+//    print("Name: Md. Mowahibur Rahman Twake\nEmail: twake.edu@gmail.com\n")
+//    print("Thank you!\n")
+//}
+
+//fun main() {
+//    var a = 15
+//    println(a)
+//
+//
+//    a =20
+//    println(a)
+//
+//    val x = 500
+//    println(x)
+//}
