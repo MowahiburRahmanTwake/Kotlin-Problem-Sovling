9f90099bc887c6908867c3bbdc951045454e690e
twake1234 <twake.edu@gmail.com>
2023-11-22 08:53:10 +0600

    add some code

---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,132 @@
+//fun main() {
+//    val num1 = 12.5
+//    val num2 = 3.5
+//    var result: Double
+//
+//    result = num1 + num2
+//    println("number1 + number2 = $result")
+//
+//    result = num1 - num2
+//    println("number1 - number2 = $result")
+//
+//    result = num1 * num2
+//    println("number1 * number2 = $result")
+//
+//    result = num1 / num2
+//    println("number1 / number2 = $result")
+//
+//    result = num1 % num2
+//    println("number1 % number2 = $result")
+//
+//
+////    result = num1 + num2
+////    print(result)
+//}
+//fun main() {
+//    var start = "Talk is cheap. "
+//    var middle = "show me the code"
+//    var end = "_ Linus Torvalds"
+//    var result = start + middle + end
+//    print(result)
+//}
+
+//fun main() {
+//    var num1:Double = 5.0
+//    var num2:Double = 2.0
+//
+//    var add = num1 + num2
+//    var sub = num1 - num2
+//    var malti = num1 * num2
+//    var div = num1 / num2
+//    var mod = num1 % num2
+//
+//    print("Addition: ${add}\n")
+//    print("Subtraction: ${sub}\n")
+//    print("Multiplication: ${malti}\n")
+//    print("Division: ${div}\n")
+//    print("Modulus: ${mod}\n")
+//}
+
+//fun main() {
+////    var number = 12
+////    number *= 5
+////    print("number = ${number}\n")
+//    var a = 11.0
+//    var b = 5.0
+//    a +=b
+//    print(a)
+//}
+
+//fun main(args: Array<String>) {
+//    val a = 1
+//    val b = true
+//    var c = 1
+//    var result: Int
+//    var booleanResult: Boolean
+//    result = -a
+//    println("-a = $result")
+//    booleanResult = !b
+//    println("!b = $booleanResult")
+//    --c
+//    println("--c = $c")
+//}
+//fun main() {
+//    var a = 1
+//    var b = true
+//    println(+a)
+//    println(-a)
+//    println(!b)
+//    ++a
+//    println(a)
+//    a--
+//    println(a)
+//}
+
+//fun main() {
+//    val a = -12
+//    val b = 12
+//// use of greater than operator
+//    val max = if (a > b) {
+//        println("a is larger than b.")
+//        a
+//    } else {
+//        println("b is larger than a.")
+//        b
+//    }
+//    println("max = $max")
+//}
+
+//fun main() {
+//    var a = 10
+//    var b = 20
+//
+//    println(a>b)
+//    println(a<b)
+//    println(a>=b)
+//    println(a<=b)
+//    println(a==b)
+//    println(a!=b)
+//}
+//fun main(args: Array<String>) {
+//    val a = 10
+//    val b = 9
+//    val c = -1
+//    val result: Boolean
+//
+//    result = (a>b) && (a>c)
+//    println(result)
+//}
+
+//fun main(args: Array<String>) {
+//    val nubmers = intArrayOf(1, 4, 42, -3)
+//    if (4 in nubmers){
+//        println("numbers array contains 4.")
+//    }
+//}
+
+fun main(args: Array<String>) {
+    val a = intArrayOf(1, 2, 3, 4, - 1)
+    println(a[1])
+    a[1]= 12
+    println(a[1])
+}
