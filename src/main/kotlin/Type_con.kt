a196cfcc0ca1b1e17a8116fcfba09cb5b540e579
twake1234 <twake.edu@gmail.com>
2023-11-22 11:02:57 +0600

    class 6

---
+++ src/main/kotlin/Type_con.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var a:Int = 10
+    println(a)
+
+    var b:Double = a.toDouble()
+    println(b)
+
+    var c:Int = 125465
+    println(c.toByte())
+    var z:String = "11232"
+    println(z.toInt())
+    println(z.toDouble())
+    var y:Char = 'A'
+    println(y.toInt())
+}
