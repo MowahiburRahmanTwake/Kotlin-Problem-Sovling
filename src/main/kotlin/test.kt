3d7120ca25781693a26ff0a63d6113b44d643216
Md. Mowahibur Rahman Twake <twake.edu@gmail.com>
2023-11-25 18:02:07 +0600

    add some code

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,7 +1,48 @@
-fun main(args : Array<String>) {
-    val number1: Int = 545344225
-    val number2: Byte = number1.toByte()
-    println("number1 = $number1")
-    println("number2 = $number2")
-}
+//fun main(args : Array<String>) {
+//    val number1: Int = 545344225
+//    val number2: Byte = number1.toByte()
+//    println("number1 = $number1")
+//    println("number2 = $number2")
+//}
+//
 
+//import java.util.Scanner
+
+//fun main(args: Array<String>) {
+//    var sc =Scanner(System.`in`)
+//    var name =sc.nextInt()
+//
+//    println("My name is ${name}")
+//}
+
+//fun main(args: Array<String>) {
+//    var sc = Scanner(System.`in`)
+//    var number1 = sc.nextInt()
+//    var number2 = sc.nextInt()
+//    var number3 = sc.nextInt()
+//
+//    if (number1>number2 && number1 > number3){
+//        println("number1 is grather")
+//    } else if (number2 > number1 && number2 > number3){
+//        println("number2 is grather")
+//
+//    }else if (number3 > number1 && number3 >number2){
+//        println("number3 is grather")
+//    }else{
+//        println("All number are equal")
+//    }
+
+//    var result = number1 + number2 + number3
+//    println("number1: ${number1}, number2: ${number2} and number3: ${number3} result is = ${result} ")
+//}
+
+fun main(args: Array<String>) {
+    var student = arrayOf("Twake","Wahidul","Imran","Mursalin")
+//    println(student[0])
+    var index = 2
+    while (index <= student.size){
+        println(student[index])
+        index++
+
+    }
+}
