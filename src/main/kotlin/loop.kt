5032f675567d03d35cd5fb550286be1255262e81
Md. Mowahibur Rahman Twake <twake.edu@gmail.com>
2023-11-26 07:59:41 +0600

    add loop

---
+++ src/main/kotlin/loop.kt
@@ -0,0 +1,64 @@
+//fun main(args: Array<String>) {
+//    var i = 1
+//    while (i<+5){
+//        println("Line $i")
+//        i++
+//    }
+//}
+// Program to compute the sum of natural numbers from 1 to 100.
+//fun main() {
+//    var sum = 0
+//    var i = 1
+//
+//    while (i <= 100) {
+//        println(i)
+//        sum += i
+//        i++
+//    }
+//    println("Sum = ${sum}")
+//}
+
+//fun main() {
+//    var sum = 0
+//    var i = 100
+//    while (i != 0){
+//        println(i)
+//        sum += i
+//        --i
+//    }
+//    println("Sum = $sum")
+//}
+
+//The program below calculates the sum of numbers entered by the user until user enters
+//0.
+
+
+//fun main() {
+//    var sum:Int = 0
+//    var input: String
+//    do {
+//        println("Enter an integer: ")
+//        input = readln()!!
+//        sum += input.toInt()
+//    }while (input != "0")
+//    println("Sum = ${sum}")
+//}
+
+//fun main() {
+//    for (i in 1 .. 5){
+//        println(i)
+//    }
+//}
+//for loop
+//fun main() {
+//    for (i in 1..10) println(i)
+//}
+
+//Different Ways to Iterate Through a Range
+
+
+//fun main() {
+//    print("for (i in 1..5) print(i) = ")
+//    for (i in 1..5) print(i)
+//    println()
+//}
--- src/main/kotlin/while.kt
+++ src/main/kotlin/while.kt
@@ -6,12 +6,20 @@
 //    }
 //}
 
-fun main(args: Array<String>) {
-    var name = arrayOf("Rahat","Sahad","Twake","Fahim","Imran","kazi","Mursalin")
-    var number = 5
+//fun main(args: Array<String>) {
+//    var name = arrayOf("Rahat","Sahad","Twake","Fahim","Imran","kazi","Mursalin")
+//    var number = 5
+//
+//    while (number < name.size){
+//        println(name[number])
+//        number ++
+//    }
+//}
 
-    while (number < name.size){
-        println(name[number])
-        number ++
+fun main(args: Array<String>) {
+    var i = 1
+    while (i <= 5) {
+        println("Line $i")
+        ++i
     }
 }
